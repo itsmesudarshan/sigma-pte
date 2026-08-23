b10c77f86cd4fe3f3b0c12e191111cbb58111cd3
itsmesudarshan <acharyasudarshan001@gmail.com>
2026-08-23 22:13:56 +0545

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Reading from './pages/Reading';
 import Writing from './pages/Writing';
+import Speaking from './pages/Speaking';
+import Listening from './pages/Listening';
 import QuestionBank from './pages/QuestionBank';
 import Practice from './pages/Practice';
 
@@ -14,6 +16,8 @@ export default function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/reading" element={<Reading />} />
           <Route path="/writing" element={<Writing />} />
+          <Route path="/speaking" element={<Speaking />} />
+          <Route path="/listening" element={<Listening />} />
           <Route path="/question-bank" element={<QuestionBank />} />
           <Route path="/practice/:id" element={<Practice />} />
         </Route>
