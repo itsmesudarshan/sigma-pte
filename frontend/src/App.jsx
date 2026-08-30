9dbb7bd2da457d412711d946611b1d274d08d64b
itsmesudarshan <acharyasudarshan001@gmail.com>
2026-08-30 06:48:53 +0545

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
 import Login from './pages/Login';
@@ -15,23 +16,25 @@ import MockTestRunner from './pages/MockTestRunner';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/reading" element={<Reading />} />
-            <Route path="/writing" element={<Writing />} />
-            <Route path="/speaking" element={<Speaking />} />
-            <Route path="/listening" element={<Listening />} />
-            <Route path="/question-bank" element={<QuestionBank />} />
-            <Route path="/practice/:id" element={<Practice />} />
-            <Route path="/mock-test" element={<MockTest />} />
-            <Route path="/mock-test/run" element={<MockTestRunner />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/reading" element={<Reading />} />
+              <Route path="/writing" element={<Writing />} />
+              <Route path="/speaking" element={<Speaking />} />
+              <Route path="/listening" element={<Listening />} />
+              <Route path="/question-bank" element={<QuestionBank />} />
+              <Route path="/practice/:id" element={<Practice />} />
+              <Route path="/mock-test" element={<MockTest />} />
+              <Route path="/mock-test/run" element={<MockTestRunner />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
